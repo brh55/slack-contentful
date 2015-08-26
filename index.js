d8cd13b264af4f2b82d6ae59c8fa94457189a20e
Brandon Him <bhim@anthropologie.com>
2015-08-25 23:03:27 -0400

    Add start script to package.json

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 'use strict';
 
+// Default Set-up
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 5000;
 
-app.get('/', function(req, res) {
+// Additional Libs
+var bodyParser = require('body-parser');
+
+var jsonParser = bodyParser.json({type: 'application/*'});
+
+app.post('/', jsonParser, function(req, res) {
+    if (!req.body) return res.send('nothing in body');
+    console.log('received request');
+    console.log(req.body);
     console.log(req);
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(port, function() {
     var host = server.address().address;
     var port = server.address().port;
 
