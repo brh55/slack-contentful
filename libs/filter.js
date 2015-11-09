3b5234b21ad0971daf42129269fda085eeb16cbc
Brandon Him <bhim@anthropologie.com>
2015-11-09 14:25:47 -0500

    Add filtered list of contentful entries

---
+++ libs/filter.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Declare Contentful entries of what you want to be notified of
+module.exports = {
+    entries: [
+        //'example entry 1',
+        //'example entry 2',
+        //'example entry 3'
+    ]
+};
