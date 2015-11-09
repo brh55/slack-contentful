eae24a27f3193e7ec035db7d09279001cd847148
Brandon Him <bhim@anthropologie.com>
2015-11-09 14:22:59 -0500

    Update comments and start of utils

--- libs/util.js
+++ libs/util.js
@@ -1,6 +1,14 @@
 'use strict';
 
+var config = require('./config');
+var settings = require('./settings');
+
 module.exports = (function () {
+    /**
+     * Format date of ISO Date
+     * @param  {[string]} ISODate [ISODate format]
+     * @return {[string]}         [returns in format of: MonthDate, Hour:Mins PM / AM]
+     */
     var formatDate = function(ISODate) {
         var tempDate = ISODate.replace(/T/, ' ').replace(/\..+/, '');
         var dateSplit = tempDate.split(' ');
@@ -35,7 +43,46 @@ module.exports = (function () {
         }
     };
 
+    /**
+     * Build message attachment to provide to slack
+     * @param  {[string]} message [string]
+     * @return {[array]}         [attachment array containing message]
+     */
+    var buildMsg = function (message) {
+        var attachment = [];
+        var messageObj = {
+            "fallback": "Required plain-text summary of the attachment.",
+            "color": "#36a64f",
+            "pretext": "Optional text that appears above the attachment block",
+            "author_name": "Bobby Tables",
+            "author_link": "http://flickr.com/bobby/",
+            "author_icon": "http://flickr.com/icons/bobby.jpg",
+            "title": "Slack API Documentation",
+            "title_link": "https://api.slack.com/",
+            "text": "Optional text that appears within the attachment"
+        };
+
+
+        attachment.push(messageObj);
+        return attachment;
+    }
+
+    /**
+     * Compare incoming hook entry against entry list
+     * @param  {[string]} entryId [entryId in question]
+     * @return {[boolean]}         [return if entry should be notified or not]
+     */
+    var checkEntry = function (entryId) {
+        if (settings.entries.indexOf(entryId) > 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     return {
-        formatDate: formatDate
+        formatDate: formatDate,
+        buildMsg: buildMsg,
+        checkEntry: checkEntry
     }
 })();
