bac2d17aff8abf3c41c7e02f9ecf499610677774
Brandon Him <brh55@drexel.edu>
2015-11-14 18:06:23 -0500

    First unit test for messageCtrl
    Update failing test

---
+++ tests/messsageCtrlTest.js
@@ -0,0 +1,43 @@
+var messageCtrlTest = require('nodeunit').testCase;
+var messageCtrl = require('../libs/messageCtrl');
+
+var mockBody = {
+	sys:
+	  { space: {
+	  		sys: {
+	  			id: "test"
+	  		}
+	  	},
+	    type: 'Entry',
+	    contentType: {
+	    	type: 'Link',
+     		linkType: 'ContentType',
+     		id: '5SAPoBZF5Y0OuuEGaumi0m0'
+     	},
+	    id: '6aFz3qcuPe0eA8kwQm0U-test',
+	    revision: 189,
+	    createdAt: '2015-08-26T02:59:44.988Z',
+	    updatedAt: '2015-11-14T20:41:49.493Z' },
+	 fields: {
+	 	name: {
+	 		'en-US': "Test Title"
+	 	},
+	    sections: [Object],
+	    morefield: [Object],
+	    moorestuff: [Object]
+	},
+	length: undefined,
+	read: [Function]
+};
+
+module.exports = messageCtrlTest({
+	"buildMessage": function (test) {
+		var message = messageCtrl.buildMessage(mockBody);
+
+		test.equal(typeof message, "object");
+		test.equal(message.channel, "#bot-testing");
+		test.equal(message.attachments[0].title_link, "https://app.contentful.com/spaces/test/entries/6aFz3qcuPe0eA8kwQm0U-test");
+		test.equal(message.attachments[0].title, "Test Title");
+		test.done();
+	}
+});
