b3b9a09f358f655aacdb02e2e0b5c5c0d131ae07
Brandon Him <bhim@anthropologie.com>
2015-11-18 08:48:18 -0500

    Add filter unit test and fix filter function

--- libs/filter.js
+++ libs/filter.js
@@ -18,7 +18,7 @@ module.exports = (function () {
          */
         getEntries: function () {
             if (action.isEntryDefined()) {
-                var formatString = model.entryString.replace(' ', '');
+                var formatString = model.entryString.replace(/\040/g, '');
                 model.entryList = formatString.split(',');
             } else {
                 console.log('No proper entries declared in .env file');
---
+++ tests/filterTest.js
@@ -0,0 +1,37 @@
+var utilTest = require('nodeunit').testCase;
+var filter = require('../libs/filter');
+
+// Using example.env as env for testing environment
+
+module.exports = utilTest({
+    checkEntry: function (test) {
+        var testTruth = filter.checkEntry('6OFbybzxM4WOCuIO4qo8Qs');
+        var testFalse = filter.checkEntry('notarealentry');
+
+        test.strictEqual(testTruth, true);
+        test.strictEqual(testFalse, false);
+
+        test.done();
+    },
+    isEntryDefined: function (test) {
+        var defineState = filter.isEntryDefined();
+
+        test.strictEqual(defineState, true);
+        test.done();
+    },
+    getEntryString: function (test) {
+        var entryString = filter.getEntryString();
+
+        test.strictEqual(entryString, '6OFbybzxM4WOCuIO4qo8Qs, 6aFz3qcuPe0eA8kwQm0Ume, entryID3, etc');
+
+        test.done();
+    },
+    getEntries: function (test) {
+        var entryArray = filter.getEntries();
+        var expectedEntries = ['6OFbybzxM4WOCuIO4qo8Qs', '6aFz3qcuPe0eA8kwQm0Ume', 'entryID3', 'etc'];
+
+        // Pretty unreliable, will probably redo to loop
+        test.equal(entryArray.toString(), expectedEntries.toString());
+        test.done();
+    }
+});
